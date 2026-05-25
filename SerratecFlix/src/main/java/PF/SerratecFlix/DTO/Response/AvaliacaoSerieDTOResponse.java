ee0a6cc0f2945b4e2c33c06f38b62c3750d68a8a
Adriane Tranhaqui <tranhaquia@gmail.com>
2026-05-25 15:27:51 -0300

    feat: adicionando informações ao DTOResponse AvaliacaoSerie

--- SerratecFlix/src/main/java/PF/SerratecFlix/DTO/Response/AvaliacaoSerieDTOResponse.java
+++ SerratecFlix/src/main/java/PF/SerratecFlix/DTO/Response/AvaliacaoSerieDTOResponse.java
@@ -1,4 +1,29 @@
 package PF.SerratecFlix.DTO.Response;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AvaliacaoSerieDTOResponse {
+	
+	private UUID id;
+    private Double nota;
+    private String comentario;
+    private LocalDateTime dataAvaliacao;
+    private UUID usuarioId;
+    private String nomeUsuario;
+    private UUID serieId;
+    private String tituloSerie;
+ 
+    public AvaliacaoSerieDTOResponse() {
+    }
+ 
 }
