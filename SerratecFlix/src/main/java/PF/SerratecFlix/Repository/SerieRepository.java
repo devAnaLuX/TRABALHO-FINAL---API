bbf500cfd9c16f1cbe5943a725fb8f5df41808a3
Adriane Tranhaqui <tranhaquia@gmail.com>
2026-05-25 15:32:41 -0300

    feat: adicionando informações ao repository Serie

--- SerratecFlix/src/main/java/PF/SerratecFlix/Repository/SerieRepository.java
+++ SerratecFlix/src/main/java/PF/SerratecFlix/Repository/SerieRepository.java
@@ -1,11 +1,15 @@
 package PF.SerratecFlix.Repository;
 
+import PF.SerratecFlix.DTO.Response.SerieDTOResponse.SerieResponseDTO;
 import PF.SerratecFlix.Domain.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.UUID;
 
 @Repository
 public interface SerieRepository extends JpaRepository<Serie, UUID> {
+
+	Collection<SerieResponseDTO> findByCategoriaId(UUID categoriaId);
 }
