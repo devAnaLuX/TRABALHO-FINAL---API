05dd4650157d69c3e89d3a1496d558057701556a
Adriane Tranhaqui <tranhaquia@gmail.com>
2026-05-25 15:24:22 -0300

    feat: adicionando informações ao repository AvaliacaoSerie

--- SerratecFlix/src/main/java/PF/SerratecFlix/Repository/AvaliacaoSerieRepository.java
+++ SerratecFlix/src/main/java/PF/SerratecFlix/Repository/AvaliacaoSerieRepository.java
@@ -4,8 +4,15 @@ import PF.SerratecFlix.Domain.AvaliacaoSerie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AvaliacaoSerieRepository extends JpaRepository<AvaliacaoSerie, UUID> {
+	
+	List<AvaliacaoSerie> findBySerieId(UUID serieId);
+	 
+    List<AvaliacaoSerie> findByUsuarioId(UUID usuarioId);
+ 
+    boolean existsByUsuarioIdAndSerieId(UUID usuarioId, UUID serieId);
 }
