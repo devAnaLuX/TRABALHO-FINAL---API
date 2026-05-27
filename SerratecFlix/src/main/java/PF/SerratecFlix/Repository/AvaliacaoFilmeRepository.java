d779f1ec5e07c86cc4e12bf5f78b49bc0f85aaba
Seu Nome <melissa1996lima@gmail.com>
2026-05-27 15:45:28 -0300

    Refactor: modifica AvaliacaoFilmeRepository

--- SerratecFlix/src/main/java/PF/SerratecFlix/Repository/AvaliacaoFilmeRepository.java
+++ SerratecFlix/src/main/java/PF/SerratecFlix/Repository/AvaliacaoFilmeRepository.java
@@ -3,10 +3,18 @@ package PF.SerratecFlix.Repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import PF.SerratecFlix.Domain.AvaliacaoFilme;
 
 @Repository
 public interface AvaliacaoFilmeRepository extends JpaRepository<AvaliacaoFilme, UUID> {
+	
+	@Query("SELECT SUM(a.nota) FROM AvaliacaoFilme a WHERE a.filme.id = :filmeId")
+	Double somarNotasDoFilme(@Param("filmeId") UUID filmeId);
+	
+	@Query("SELECT COUNT(a) FROM AvaliacaoFilme a WHERE a.filme.id = :filmeId")
+	Long contarAvaliacoesDoFilme(@Param("filmeId") UUID filmeId);
 }
